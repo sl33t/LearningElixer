24e6c66cfddc91689ea2bfadb4044e34a09df2f6
rcatron <catron.ricky@gmail.com>
2016-07-15 14:52:39 -0400

    Added palindrome support

--- lib/elixerprograms.ex
+++ lib/elixerprograms.ex
@@ -13,10 +13,12 @@ defmodule Elixerprograms do
 		input = "#{options[:input]}"
 		
 		case program do
-			"reversestring" -> IO.puts recursive_reverse(input, "")
+			"reversestring" -> IO.puts recursive_reverse(input)
+			"palindrome" -> IO.puts palindrome(input)
 			_ -> IO.puts "No program specified." <>
 						 "\n\t Option: --program" <>
-						 "\n\t Values: reversestring"
+						 "\n\t Values: reversestring" <>
+						 "\n\t Values: palindrome"
 		end
 	end
 	
@@ -29,6 +31,17 @@ defmodule Elixerprograms do
 							]
 						  )
 		options
+	end
+	
+	def palindrome(input) do
+		cond do
+			recursive_reverse(input) == input -> "Is palindrome"
+			true -> "Not palindrome"
+		end
+	end
+	
+	def recursive_reverse(input) do
+		recursive_reverse(input, "")
 	end
 	
 	def recursive_reverse("", reverse_string) do
